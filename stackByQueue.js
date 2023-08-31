d5eee7adc9a02fef8491aa54a57d2ed1a3c97817
deepaknagar62 <nagardeepak9098@gmail.com>
2023-08-31 18:55:39 +0530

    Stack using java

--- stackByQueue.js
+++ stackByQueue.js
@@ -30,3 +30,11 @@ MyStack.prototype.empty = function() {
    return this.q.length === 0;
 };
 
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
