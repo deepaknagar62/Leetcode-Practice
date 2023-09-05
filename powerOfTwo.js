9c95928d118093f89c47a3817212d7bf335108e4
deepaknagar62 <nagardeepak9098@gmail.com>
2023-09-05 18:51:07 +0530

    new file

---
+++ powerOfTwo.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n==1) return true;
+    if(n==0) return false;
+    if(n%2 != 0) return false;
+    else{
+        return isPowerOfTwo(n/2);
+    }
+};
